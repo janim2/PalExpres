09435ded65aeb61b504a24ef3c169fac802a5704
Jesse <45318633+janim2@users.noreply.github.com>
2019-04-26 10:02:01 +0000

    Update config.php

--- Pal Express/config.php
+++ Pal Express/config.php
@@ -6,7 +6,7 @@
 */
 
 try {
-	$con /*just a variable to hold the connection and use in all pages*/ = new PDO('mysql:host=localhost; dbname=id2958837_turnup', 'id2958837_turnup'/*username*/, 'Turnup'/*password*/);
+	$con /*just a variable to hold the connection and use in all pages*/ = new PDO('mysql:host=localhost; dbname='/database name here/', '/username here/'/*username*/, '/passsword here/'/*password*/);
 	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //This prevents PDO from displaying the default error and the error's behaviour
 }catch (PDOException $e/*This catches an exception or error when trying to connect to database and casts the error to $e*/){
 	die("Sorry... Encountered a database problem."); //die kills the page. ie. the rest of the page won't work when you require or include in php.
